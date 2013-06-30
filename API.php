f791b0fb8f303c1b3e66c923f287162140cd09b6
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2013-06-30 17:01:35 +0800

    added MYSQL Database for persistent URL storage

---
+++ API.php
@@ -0,0 +1,48 @@
+<?php
+
+
+$mode = mysql_real_escape_string($_GET['mode']);
+$databaseName = 'playlists';
+
+//Create connection
+$connection =  mysql_connect('localhost','root','');
+mysql_set_charset('UTF-8');
+if(!$connection)
+	{
+		die("Failed");
+	}
+$db_selected = mysql_select_db($databaseName,$connection);
+if(!$db_selected)
+	{
+    	die('Could not find the database' .mysql_error());
+	}
+
+
+if($mode == 'write')
+	{	
+		$htmlDataString = mysql_real_escape_string($_GET['htmlData']);
+		$query1="SELECT id FROM userplaylists ORDER BY id DESC LIMIT 1";
+		$lastId = array();
+		$lastEntry = mysql_query($query1); 
+		$lastId = mysql_fetch_array($lastEntry);
+		$lastIdInt = "$lastId[id]";
+		$currentId = intval($lastIdInt) + intval('1');
+		$query2 = "INSERT INTO userplaylists(id,htmlData) VALUES ('$currentId','$htmlDataString')";
+		if(!mysql_query($query2,$connection))
+		    	{
+		        	die("At2, Error occured" . mysql_error());
+		    	}
+    
+    }
+else if($mode == 'read')
+{
+	$playlistId = mysql_real_escape_string($_GET['id']);
+	$query = "SELECT htmlData FROM userplaylists WHERE id='$playlistId'";
+	$result = mysql_query($query);
+	$resultArray = array();
+	$resultArray = mysql_fetch_array($result);
+	$playlistHTML = "$resultArray[htmlData]";
+	echo $playlistHTML;
+
+}
+?>
--- restAPI.php
+++ restAPI.php
@@ -1,10 +1,7 @@
 
 <?php
+//This API stores the user playlists in a locally stored html file and returns URL to callback function
 //script retrieves html data from index.html and creates a .html with playlist
-//echo ($POST);
-
-
-
 $playlistNumber = 0;
 $file = fopen("playlistCount.txt", "r") or exit("Unable to open file!");
 //Getting current playlist number
@@ -24,5 +21,7 @@ $extension ='.html';
 $handle = fopen($fileName.$playlistNumber.$extension,"w+");
 fwrite($handle,"<div>".$_POST['htmlData']."</div>");
 fclose($handle);
-
+//returning URL
+$URL = $fileName.$playlistNumber.$extension;
+echo $URL;
 ?>
